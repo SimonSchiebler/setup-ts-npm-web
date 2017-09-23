51b66f5e54338123f95e090a97f62e5cab34b1c9
Unknown <robin42hartmann@gmail.com>
2017-09-23 18:59:13 +0200

    Fixed VSCode debugging not working
    - typescript outDir is now being configured by gulp
    - added sourcemaps using gulp-sourcemaps
    - configured tsconfig.json

--- gulp/compile.js
+++ gulp/compile.js
@@ -2,16 +2,19 @@ const config = require('./config');
 
 const gulp = require('gulp');
 const series = require('run-sequence');
+const sourcemaps = require('gulp-sourcemaps');
 const ts = require('gulp-typescript');
 
-const tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json', { outDir: config.paths.compile });
 
 const compilablesGlob = 'src/**/*.{ts,js}';
 const othersSelector = ['src/**/*', `!${compilablesGlob}`];
 
 const compile = () =>
   gulp.src(compilablesGlob)
+    .pipe(sourcemaps.init())
     .pipe(tsProject())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.paths.compile));
 
 gulp.task('prepare:compile', done =>
