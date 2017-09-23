37ac6a39120f7bea4b3e76a97af85abb27b74437
Unknown <robin42hartmann@gmail.com>
2017-09-23 13:35:44 +0200

    improved linting - added eslint-config-airbnb-base - configure eslint config - removed 'use strict' from gulp files

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,4 +1,3 @@
-'use strict';
 
 const gulp = require('gulp');
 const clean = require('gulp-clean');
--- gulp/serve.js
+++ gulp/serve.js
@@ -1,4 +1,3 @@
-'use strict';
 
 const gulp = require('gulp');
 const shell = require('gulp-shell');
